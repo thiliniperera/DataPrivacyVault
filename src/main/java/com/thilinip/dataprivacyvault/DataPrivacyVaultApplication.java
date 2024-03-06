0e2a61e5d90fe81c71785d803a0a44d48486e529
Thilini Perera <thiliniperera9@gmail.com>
2024-03-06 19:25:07 +0100

    Initial commit

---
+++ src/main/java/com/thilinip/dataprivacyvault/DataPrivacyVaultApplication.java
@@ -0,0 +1,13 @@
+package com.thilinip.dataprivacyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPrivacyVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPrivacyVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thilinip/dataprivacyvault/DataPrivacyVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilinip.dataprivacyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPrivacyVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
